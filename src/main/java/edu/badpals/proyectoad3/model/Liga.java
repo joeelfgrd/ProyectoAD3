5d7181f53215e010d31194d99afe745f20e84e5b
Joel <joelfigueiridomolares@gmail.com>
2024-12-28 18:25:58 +0100

    Feat(Liga.java):Crear clase liga con atributos

---
+++ src/main/java/edu/badpals/proyectoad3/model/Liga.java
@@ -0,0 +1,28 @@
+package edu.badpals.proyectoad3.model;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "Liga")
+public class Liga {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_liga;
+
+    @Column(name = "Nombre", nullable = false)
+    private String nombre;
+
+    @Column(name = "fecha_creacion", nullable = false)
+    private LocalDate fecha_creacion;
+
+    @Column(name = "region", nullable = false)
+    private String region;
+
+    @Column(name = "Tier", nullable = false)
+    private String Tier;
+
+
+
+}
