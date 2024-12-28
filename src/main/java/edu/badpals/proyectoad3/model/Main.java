356fe384559b3e974866f9bf9ea042f9de491b6f
Joel <joelfigueiridomolares@gmail.com>
2024-12-28 19:25:47 +0100

    Fix(Persistence.xml):Arreglado para funcionar con la base de datos

---
+++ src/main/java/edu/badpals/proyectoad3/model/Main.java
@@ -0,0 +1,49 @@
+package edu.badpals.proyectoad3.model;
+
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+
+        try {
+            Liga liga = new Liga();
+            liga.setNombre("Liga de Ejemplo");
+            liga.setFechaCreacion(LocalDate.of(2020, 1, 1));
+            liga.setRegion("EU");
+            liga.setTier("S");
+
+
+
+            Equipo equipo = new Equipo();
+            equipo.setNombre("Equipo de Prueba");
+            equipo.setFechaCreacion(LocalDate.of(2015, 5, 15));
+            equipo.setRegion("EU");
+            equipo.setTier("A");
+
+
+            EquipoLiga equipoLiga = new EquipoLiga();
+            equipoLiga.setEquipo(equipo);
+            equipoLiga.setLiga(liga);
+            equipoLiga.setFechaInscripcion(LocalDate.of(2022, 10, 10));
+            equipoLiga.setPrecioPlaza(5000.00);
+
+
+            em.persist(liga);
+            em.persist(equipo);
+            em.persist(equipoLiga);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            em.getTransaction().rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+            emf.close();
+        }
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,10 @@ module edu.badpals.proyectoad3 {
     requires javafx.controls;
     requires javafx.fxml;
     requires jakarta.persistence;
-
+    requires org.hibernate.orm.core;
 
     opens edu.badpals.proyectoad3 to javafx.fxml;
     exports edu.badpals.proyectoad3;
     exports edu.badpals.proyectoad3.model;
-    opens edu.badpals.proyectoad3.model to javafx.fxml;
+    opens edu.badpals.proyectoad3.model to javafx.fxml, org.hibernate.orm.core;
 }
