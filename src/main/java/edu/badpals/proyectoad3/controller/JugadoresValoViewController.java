948b4447f70429821095f97afcf56d80a6b09afc
Manuerty <manuelcendonr@gmail.com>
2025-01-07 20:04:33 +0100

    feat(JUgadoresValoViewController): finish the comboboxs

--- src/main/java/edu/badpals/proyectoad3/controller/JugadoresValoViewController.java
+++ src/main/java/edu/badpals/proyectoad3/controller/JugadoresValoViewController.java
@@ -1,16 +1,61 @@
 package edu.badpals.proyectoad3.controller;
 
+import edu.badpals.proyectoad3.model.Conection_App;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.List;
 
 public class JugadoresValoViewController {
+
+    @FXML
+    private final Conection_App conectionApp = new Conection_App();
+
+    @FXML
+    private Button VolverValoPlayerBtn;
+
+    @FXML
+    private TextField ValoPlayerNickTxt;
+
+    @FXML
+    private ComboBox ValoPlayerRolCmb;
+
+    @FXML
+    private ComboBox ValoPlayerTeamCmb;
+
+    @FXML
+    private ComboBox ValoPlayerCountryCmb;
+
+    @FXML
+    private TextField ValoPlayerSurnameTxt;
+
+    @FXML
+    private TextField ValoPlayerNameTxt;
+
+    @FXML
+    private Button DeleteValoPlayerBtn;
+
+    @FXML
+    private Button UpdateValoPlayerBtn;
+
+    @FXML
+    private Button CreateValoPlayerBtn;
+
+    @FXML
+    private CheckBox ValoPlayerLateShtCllrChk;
+
+    @FXML
+    private ListView ValoPlayerListView;
+
+    @FXML
+    private ComboBox ValoPlayerAgentCmb;
 
     @FXML
     public void toMainMenu(ActionEvent event) throws IOException {
@@ -28,5 +73,16 @@ public class JugadoresValoViewController {
         // Cerrar la ventana actual
         Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         currentStage.close();
+    }
+
+    @FXML
+    public void initialize() {
+        List<String> lista = Conection_App.returnAllTeams(conectionApp.crearConexion());
+        ValoPlayerRolCmb.getItems().addAll("Duelista", "Iniciador", "Controlador", "Centinela");
+        ValoPlayerCountryCmb.getItems().addAll("Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cabo Verde", "Cambodia", "Cameroon", "Canada", "Central African Republic", "Chad", "Chile", "China", "Colombia", "Comoros", "Congo (Congo-Brazzaville)", "Costa Rica", "Croatia", "Cuba", "Cyprus", "Czechia (Czech Republic)", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Eswatini (fmr. Swaziland)", "Ethiopia", "Fiji", "Finland", "France", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Grenada", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Holy See", "Honduras", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea (North)", "Korea (South)", "Kosovo", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar (Burma)", "Namibia", "Nauru", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Macedonia", "Norway", "Oman", "Pakistan", "Palau", "Palestine State", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Qatar", "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Sudan", "Spain", "Sri Lanka", "Sudan", "Suriname", "Sweden", "Switzerland", "Syria", "Tajikistan", "Tanzania", "Thailand", "Timor-Leste", "Togo", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States of America", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Yemen", "Zambia", "Zimbabwe");
+        ValoPlayerTeamCmb.getItems().addAll(lista);
+        ValoPlayerAgentCmb.getItems().addAll("Astra", "Breach", "Brimstone", "Chamber", "Cypher", "Fade", "Gekko", "Harbor", "Jett", "Killjoy", "KAY/O", "Neon", "Omen", "Phoenix", "Raze", "Reyna", "Sage", "Skye", "Sova", "Tejo", "Viper", "Vyse", "Yoru"
+        );
+
     }
 }
