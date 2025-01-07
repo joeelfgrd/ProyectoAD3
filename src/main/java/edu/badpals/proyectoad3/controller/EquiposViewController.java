ce90cdcd287b3606e85a935c42abb9e4c790e46a
Joel <joelfigueiridomolares@gmail.com>
2025-01-07 17:12:57 +0100

    feat(EquiposViewController.java):Agregar funcionalidad de listar equipos

--- src/main/java/edu/badpals/proyectoad3/controller/EquiposViewController.java
+++ src/main/java/edu/badpals/proyectoad3/controller/EquiposViewController.java
@@ -1,5 +1,9 @@
 package edu.badpals.proyectoad3.controller;
 
+import edu.badpals.proyectoad3.model.Conection_App;
+import edu.badpals.proyectoad3.model.entities.Equipo;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -7,14 +11,36 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.util.List;
 
 public class EquiposViewController {
 
     @FXML
+    private TableView<Equipo> tableEquipos;
+
+    @FXML
+    private TableColumn<Equipo, String> colNombre;
+
+    @FXML
+    private TableColumn<Equipo, String> colRegion;
+
+    @FXML
+    private TableColumn<Equipo, String> colTier;
+
+    @FXML
+    private TableColumn<Equipo, String> colFechaCreacion;
+
+    @FXML
     private Button VolverTeamBtn;
+
+    private final Conection_App conectionApp = new Conection_App();
 
     @FXML
     public void toMainMenu(ActionEvent event) throws IOException {
@@ -33,4 +59,35 @@ public class EquiposViewController {
         Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         currentStage.close();
     }
+
+    @FXML
+    public void setCells() {
+        colNombre.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+        colRegion.setCellValueFactory(new PropertyValueFactory<>("region"));
+        colTier.setCellValueFactory(new PropertyValueFactory<>("tier"));
+        colFechaCreacion.setCellValueFactory(new PropertyValueFactory<>("fechaCreacion"));
+    }
+
+    @FXML
+    public void loadData() {
+        Connection connection = conectionApp.crearConexion();
+        if (connection != null) {
+            List<Equipo> equipos = Conection_App.getEquipos(connection);
+            ObservableList<Equipo> equiposObservableList = FXCollections.observableArrayList(equipos);
+            tableEquipos.setItems(equiposObservableList);
+            conectionApp.cerrarConexion(connection);
+        } else {
+            System.out.println("No se pudo establecer la conexión con la base de datos.");
+        }
+    }
+
+    @FXML
+    public void initialize() {
+        setCells();
+        loadData();
+        tableEquipos.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+    }
+
+
+
 }
