a0fa77eb60b10223b92eeb20934488dd889477c3
Joel <joelfigueiridomolares@gmail.com>
2025-01-11 10:32:58 +0100

    Feat:Conexion a cluster mongoDb creada

---
+++ src/main/java/edu/badpals/proyectoad3/DAO/MongoClientConnection.java
@@ -0,0 +1,35 @@
+package edu.badpals.proyectoad3.DAO;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoException;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+public class MongoClientConnection {
+    public static void main(String[] args) {
+        String connectionString = "mongodb+srv://accesobd:259hpvMbH0VgemMK@cluster0.unvuy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .serverApi(serverApi)
+                .build();
+        try (MongoClient mongoClient = MongoClients.create(settings)) {
+            try {
+                MongoDatabase database = mongoClient.getDatabase("admin");
+                database.runCommand(new Document("ping", 1));
+                System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
+            } catch (MongoException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+
